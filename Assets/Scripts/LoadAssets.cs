726af5b98d26e439c958f324f38bc5c244d5dc01
Kjell Iwarson <lightsoda@gmail.com>
2017-04-10 20:54:08 +0200

    Load table async

---
+++ Assets/Scripts/LoadAssets.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadAssets : MonoBehaviour {
+	void Start () {
+        SceneManager.LoadSceneAsync("Table01", LoadSceneMode.Additive);
+	}
+}
