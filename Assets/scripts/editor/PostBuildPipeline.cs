ad3a86a109fc358ab88907ee370f001bab3d613d
Stewart Martin <stewartjmartin@gmail.com>
2017-04-13 01:18:34 -0500

    Modified PostBuildPipeline.cs to put the AssetBundles folder in the correct place for OSX builds.

--- Assets/scripts/editor/PostBuildPipeline.cs
+++ Assets/scripts/editor/PostBuildPipeline.cs
@@ -1,14 +1,22 @@
-﻿// C# example:
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEditor;
 using UnityEditor.Callbacks;
+using AssetBundles;
 
 public class MyBuildPostprocessor
 {
     [PostProcessBuildAttribute(1)]
     public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
     {
-        var targetPath = pathToBuiltProject.Substring(0, pathToBuiltProject.LastIndexOf('/') + 1);
+        string targetPath;
+        if (Utility.GetPlatformName() == "OSX")
+        {
+            targetPath = pathToBuiltProject + "/";
+        }
+        else
+        {
+            targetPath = pathToBuiltProject.Substring(0, pathToBuiltProject.LastIndexOf('/') + 1);
+        }
         var sourcePath = Application.dataPath + "/../AssetBundles";
         Debug.Log("Copying AssetBundles to " + targetPath);
         FileUtil.DeleteFileOrDirectory(targetPath + "AssetBundles");
