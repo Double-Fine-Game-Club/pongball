1b615813e071f16571d34c9274a9842f2ece09f0
Kjell Iwarson <lightsoda@gmail.com>
2017-04-13 05:57:17 +0200

    Add post build copy of AssetBundles directory

---
+++ Assets/scripts/editor/PostBuildPipeline.cs
@@ -0,0 +1,16 @@
+﻿// C# example:
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+public class MyBuildPostprocessor
+{
+    [PostProcessBuildAttribute(1)]
+    public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+    {
+        var targetPath = pathToBuiltProject.Substring(0, pathToBuiltProject.LastIndexOf('/') + 1);
+        var sourcePath = Application.dataPath + "/../AssetBundles";
+        Debug.Log("Copying AssetBundles to " + targetPath);
+        FileUtil.CopyFileOrDirectory(sourcePath, targetPath + "AssetBundles");
+    }
+}
