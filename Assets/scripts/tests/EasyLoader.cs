337ba469e2f5bd47c7c3cbb6ad3cc46c3846d4fe
Stewart Martin <stewartjmartin@gmail.com>
2017-04-15 00:26:01 -0500

    All these changes are mostly a result of importing the table by @KGuNN45 and then testing that table.  I created a few prefabs from the existing scenes, so I could import them into the new scene.  I also added a simple script that loads the new scene asynchronously because I couldn't grock how to do it properly with the current infrastructure.
    I also added the existing script to the paddle prefab to test player movement of paddles, but left it disabled.

---
+++ Assets/scripts/tests/EasyLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EasyLoader : MonoBehaviour {
+
+
+	void Start () {
+        DontDestroyOnLoad(gameObject);
+		//Asset Bundle Manager confuses me, so this is just going to load the scene so I can test it.  Might not work.
+        SceneManager.LoadSceneAsync("Table02");
+	}
+	
+
+}
