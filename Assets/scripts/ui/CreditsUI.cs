c40a81d03f6f53dce63c71fd1d5b91d6614067b8
ehne <ehnevault@gmail.com>
2017-04-17 18:28:33 +1000

    credits stuff

---
+++ Assets/scripts/ui/CreditsUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsUI : MonoBehaviour {
+
+	public textasset count;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
