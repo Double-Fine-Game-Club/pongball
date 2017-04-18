8c99031badf152cc6fc5d9b0209e85bdb134b94b
Stewart Martin <stewartjmartin@gmail.com>
2017-04-18 03:46:34 -0500

    Added RHCooper's logo file, and added a Switcher to switch between them.  A random logo will appear upon load, and logos can be switched by clicking the logo. I also added a text file with directions on adding new logos to the switcher. Lastly, I added a uGUITools Editor script that's really useful when working with uGUI.  Best info I can find for it is here https://forum.unity3d.com/threads/scripts-useful-4-6-scripts-collection.264161/ I think it basically has no license.  Made by a "Senshi"  Super useful.

---
+++ Assets/scripts/ui/LogoSwitcher.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogoSwitcher : MonoBehaviour 
+{
+    public GameObject[] logoArray;
+    private int randomLogo;
+    private int currentLogo = 0;
+
+    void Start()
+    {
+        RandomLogo();
+        Debug.Log(logoArray.Length);
+    }
+
+    public void RandomLogo()
+    {
+        randomLogo = Random.Range(0, logoArray.Length-1);
+        logoArray[randomLogo].SetActive(true);
+        currentLogo = randomLogo;
+
+    }
+
+    public void NextLogo()
+    {
+        logoArray[currentLogo].SetActive(false);
+        if (currentLogo < logoArray.Length-1)
+        {
+        currentLogo++;
+        }
+        else
+        {
+            currentLogo = 0;
+        }
+        logoArray[currentLogo].SetActive(true);
+
+
+       
+    }
+}
