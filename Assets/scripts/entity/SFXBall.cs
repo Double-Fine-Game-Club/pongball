84c9cbe15c310ecddaa112d1e373e71503b64187
Stewart Martin <stewartjmartin@gmail.com>
2017-04-18 17:58:27 -0500

    Fix silly mistake

--- Assets/scripts/entity/SFXBall.cs
+++ Assets/scripts/entity/SFXBall.cs
@@ -6,18 +6,18 @@ public class SFXBall : MonoBehaviour {
     public AudioClip wallBounce;
     public AudioClip paddleBounce;
 
-    void onCollisionEnter(Collision col)
+    void OnCollisionEnter(Collision col)
     {
-        Debug.Log("Am I stupid");
+
         if (col.gameObject.tag == "Wall")
 
         {
-            Debug.Log("Wall!");
+
             SoundManager.instance.RandomizeSfx(wallBounce);
         }
         else if (col.gameObject.tag == "Paddle")
         {
-            Debug.Log("Paddle!");
+
             SoundManager.instance.RandomizeSfx(paddleBounce);
         }
     }
