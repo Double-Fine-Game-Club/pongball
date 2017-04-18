265cfb69acf49ca97125391dc24b2ab926f778d4
James Wood <jamie_wood@ymail.com>
2017-04-18 23:12:09 +0100

    Add light pad prefab

---
+++ Assets/scripts/entity/LightPad.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Lights up when a ball triggers it
+/// </summary>
+public class LightPad : MonoBehaviour {
+
+	public Color baseColour;
+	public Color lightColour;
+	private int balls;
+
+	void Start () 
+	{
+		balls = 0;
+		SetColor(baseColour);
+	}
+
+	void Update () 
+	{
+		
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (collider.tag == "Ball")
+		{
+			balls += 1;
+
+			if (balls > 0)
+			{
+				SetColor(lightColour);
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider collider)
+	{
+		if (collider.tag == "Ball")
+		{
+			balls -= 1;
+
+			if (balls <= 0)
+			{
+				SetColor(baseColour);
+			}
+		}
+	}
+
+	void SetColor(Color color)
+	{
+		GetComponent<Renderer>().material.color = color;
+		GetComponent<Renderer>().material.SetColor("_EmissionColor", color);
+	}
+}
