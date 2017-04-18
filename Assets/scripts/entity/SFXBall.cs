ec58190df8cd086571a87df39494979f3a1932bf
Stewart Martin <stewartjmartin@gmail.com>
2017-04-18 06:02:00 -0500

    Tried to get a SoundManager working.  Music works.  The collider won't trigger for SFX and I can't figure out why.  I'm running out of steam so I'm  hitting the hay.

---
+++ Assets/scripts/entity/SFXBall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXBall : MonoBehaviour {
+    public AudioClip wallBounce;
+    public AudioClip paddleBounce;
+
+    void onCollisionEnter(Collision col)
+    {
+        Debug.Log("Am I stupid");
+        if (col.gameObject.tag == "Wall")
+
+        {
+            Debug.Log("Wall!");
+            SoundManager.instance.RandomizeSfx(wallBounce);
+        }
+        else if (col.gameObject.tag == "Paddle")
+        {
+            Debug.Log("Paddle!");
+            SoundManager.instance.RandomizeSfx(paddleBounce);
+        }
+    }
+
+}
