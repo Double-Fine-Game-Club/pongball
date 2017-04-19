747800f3d38f050889993cd1637e0514178666a4
Stewart Martin <stewartjmartin@gmail.com>
2017-04-18 22:27:25 -0500

    Sound Manager (#40)
    Implements SoundManager and audio for music, UI and ball impacts on paddles and walls.
    * Tried to get a SoundManager working.  Music works.  The collider won't trigger for SFX and I can't figure out why.  I'm running out of steam so I'm  hitting the hay.
    * These stragglers showed up after I quit unity.  Dunno why.  I tried reloading the scene to see if SFX was working, by some fluke chance, but no luck.  Aliens.
    * Fix silly mistake
    * Music stutters or skips.  Profiler shows AudioManager.Update or AudioManager.FixedUpdate occasionally spiking.  Investigating.
    * Added Ui effects for SFX using Kyle3Wynn's assets.
    * Also added license header to SoundManager.cs
    * Also made a SoundManager prefab, but the prefab really doesn't have a purpose currently....I * needed it for a test I was doing.  I <3 prefabs.

---
+++ Assets/scripts/entity/SFXBall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXBall : MonoBehaviour {
+    public AudioClip wallBounce;
+    public AudioClip paddleBounce;
+
+    void OnCollisionEnter(Collision col)
+    {
+
+        if (col.gameObject.tag == "Wall")
+
+        {
+
+            SoundManager.instance.RandomizeSfx(wallBounce);
+        }
+        else if (col.gameObject.tag == "Paddle")
+        {
+
+            SoundManager.instance.RandomizeSfx(paddleBounce);
+        }
+    }
+
+}
---
+++ Assets/scripts/ui/SFXUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXUI : MonoBehaviour {
+    public AudioClip hoverOver;
+    public AudioClip onClick;
+    public void HoverOverSFX()
+    {
+        SoundManager.instance.PlaySingle(hoverOver);
+    }
+    public void OnClickSFX()
+    {
+        SoundManager.instance.PlaySingle(onClick);
+    }
+
+}
