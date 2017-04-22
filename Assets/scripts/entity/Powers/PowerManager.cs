ee776b9f1fc63e39e8282e29fbd70df2387a81f7
Michael McKenzie <mmckenzie@mun.ca>
2017-04-21 23:08:11 -0230

    Added framework superpower files.  Attached to player paddle prefab.

---
+++ Assets/scripts/entity/Powers/PowerManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerManager : MonoBehaviour {
+    private PaddleBase paddle;
+
+	// Use this for initialization
+	void Start () {
+        paddle = GetComponent<PaddleBase>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
