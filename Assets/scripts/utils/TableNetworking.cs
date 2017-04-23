6ade5bc916ffa9cf972144d2105fe8543915aa28
James Wood <jamie_wood@ymail.com>
2017-04-23 23:20:05 +0100

    Network table selection

---
+++ Assets/scripts/utils/TableNetworking.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TableNetworking : NetworkBehaviour 
+{
+	[SyncVar]
+	public string variant;
+
+	[SyncVar]
+	public string table;
+
+	[SyncVar]
+	public bool selected;
+
+	[ServerCallback]
+	void Start () 
+	{
+		selected = false;
+		table = "";
+		variant = "";
+	}
+
+	public string GetTable()
+	{
+		return table;
+	}
+
+	public string GetVariant()
+	{
+		return variant;
+	}
+
+	public bool ServerHasSelected()
+	{
+		return selected;
+	}
+
+	public void SetTableInfo(string serverVariant, string serverTable)
+	{
+		selected = true;
+		variant = serverVariant;
+		table = serverTable;
+	}
+}
