dae62f6a2efc913264b85c74aa06ce851c5ca60a
Stewart Martin <stewartjmartin@gmail.com>
2017-04-23 03:21:16 -0500

    Moar soundz (#58)
    * Apparently there was a minor big in my random selecting code.  I don't understand it, really, but apparently at has something to do with Random.Range.  Works now.  Random logo should show when game starts.  Additionally I added a line of code so that, if someone wants to they cane use the code to select a random Logo at will, but it's currently not set up like that.
    
    * Added most of the currently available SFX to sfxball.cs.  Added a bunch of tags for those objects, and tagged the objects in table2.  other tables may still need objects tagged.
    Added a scrollable options panel to Main Menu scene with sound volume and a switch to switch between the paddle SFX.
    Moved audio assets to Resources folder so they can be grabbed with Resources.LoadAll in the future.
    Did I do anything else?  I need to make more frequent commits.
    
    * Added CloseAllPanels function to close all the panels before a new one opens.

--- Assets/scripts/entity/SFXBall.cs
+++ Assets/scripts/entity/SFXBall.cs
@@ -5,21 +5,85 @@ using UnityEngine;
 public class SFXBall : MonoBehaviour {
     public AudioClip wallBounce;
     public AudioClip paddleBounce;
+    private bool TrueForKyle3wynn;
+    public AudioClip[] lBumper;
+    public AudioClip[] lPaddle;
+    public AudioClip rampUpShort;
+    public AudioClip rampUpLong;
+    public AudioClip rampDownShort;
+    public AudioClip rampDownLong;
+    public AudioClip powerUp;
+    public AudioClip goalUp;
+    public AudioClip goalDown;
+
+
+    void Start()
+    {
+        TrueForKyle3wynn = SoundManager.instance.TrueForKyle3wynn;
+    }
+
 
     void OnCollisionEnter(Collision col)
     {
+        var tag = col.gameObject.tag;
 
-        if (col.gameObject.tag == "Wall")
+        switch (tag)
+        {  
+            case "Wall":
+                SoundManager.instance.RandomizeSfx(wallBounce);
 
-        {
+                break;
+            case "Paddle":
+                if (TrueForKyle3wynn == true)
+                {
+                    SoundManager.instance.RandomizeSfx(paddleBounce);
+                }
+                else
+                {
+                    SoundManager.instance.RandomSFX(lPaddle);
+                }
+                break;
+            case "Flipper":
+                SoundManager.instance.RandomizeSfx(wallBounce);
+                break;
+            case "Bumper":
+                SoundManager.instance.RandomSFX(lBumper);
 
-            SoundManager.instance.RandomizeSfx(wallBounce);
+                break;
+            case "Goal":
+                SoundManager.instance.PlaySingle(goalUp);
+
+                break;
+            case "RampUp":
+                SoundManager.instance.PlaySingle(rampUpShort);
+
+                break;
+            case "RampDown":
+                SoundManager.instance.PlaySingle(rampDownShort);
+
+                break;
+            case "PowerUp":
+                SoundManager.instance.PlaySingle(powerUp);
+
+                break;
+            default:
+                Debug.Log("Collided with untagged object");
+                break;
         }
-        else if (col.gameObject.tag == "Paddle")
-        {
 
-            SoundManager.instance.RandomizeSfx(paddleBounce);
-        }
+
+
+//        if (col.gameObject.tag == "Wall")
+//
+//        {
+//
+//            SoundManager.instance.RandomizeSfx(wallBounce);
+//        }
+//        else if (col.gameObject.tag == "Paddle")
+//        {
+//
+//            SoundManager.instance.RandomizeSfx(paddleBounce);
+//        }
     }
 
 }
--- Assets/scripts/ui/UGUIButtons.cs
+++ Assets/scripts/ui/UGUIButtons.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UGUIButtons : MonoBehaviour {
+    public GameObject optionsPanel;
+    public GameObject creditsPanel;
+
+
 
     public void PlayGameButton()
     {
@@ -12,7 +16,28 @@ public class UGUIButtons : MonoBehaviour {
 
     public void OptionsButton()
     {
-        Debug.Log("This does nothing currently");
+        CloseAllPanels();
+        if (optionsPanel.activeSelf == false)
+        {
+            optionsPanel.SetActive(true);
+        }
+        else if (optionsPanel.activeSelf == true)
+        {
+            optionsPanel.SetActive(false);
+        }
+    }
+
+    public void CreditsButton()
+    {
+        CloseAllPanels();
+        if (creditsPanel.activeSelf == false)
+        {
+            creditsPanel.SetActive(true);
+        }
+        else if (creditsPanel.activeSelf == true)
+        {
+            creditsPanel.SetActive(false);
+        }
     }
 
     public void QuitButton()
@@ -21,4 +46,9 @@ public class UGUIButtons : MonoBehaviour {
         Application.Quit();
     }
 
+    private void CloseAllPanels()
+    {
+        optionsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+    }
 }
