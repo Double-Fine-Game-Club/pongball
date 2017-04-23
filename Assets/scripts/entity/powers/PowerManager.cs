7cb724e5bb48bd1a94e6311d4fca7f301c593c0e
James Wood <jamie_wood@ymail.com>
2017-04-23 21:02:32 +0100

    Add networking to the powers

--- Assets/scripts/entity/powers/PowerManager.cs
+++ Assets/scripts/entity/powers/PowerManager.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Networking;
 
 public class PowerManager : MonoBehaviour {
 
@@ -69,24 +69,48 @@ public class PowerManager : MonoBehaviour {
         if(Input.GetKeyDown(KeyCode.Space))
         {
             currentPower.Activate();
-            //TODO
+
             //Tell network power has activated
+			if (NetworkManager.singleton.isNetworkActive)
+			{
+				GetComponent<PaddleNetworking>().CmdUsePower();
+			}
         }
         if (oldPower!=null) oldPower.Update();
         if (currentPower!=null) currentPower.Update();
 
 	}
 
-    void giveNewPower()
+	void giveNewPower()
     {
-        oldPower = currentPower;
+		oldPower = currentPower;
+
         float randFloat = Random.Range(1, (float)(powerTypes.POWER_COUNT));
         int randInt = Mathf.FloorToInt(randFloat);
         currentPower = powerMapping[(powerTypes)randInt];
         currentPower.Ready();
 
         Debug.Log("Power Granted: " + powerMapping[(powerTypes)randInt].ToString() + " to player " + ownerId);
-        //TODO
+        
         //Tell network which power this player has
+		if (NetworkManager.singleton.isNetworkActive)
+		{
+			GetComponent<PaddleNetworking>().CmdSetPower(randInt);
+		}
     }
+
+	// Called by the server to assign a non-player with a power
+	public void AssignPower(int power)
+	{
+		oldPower = currentPower;
+
+		currentPower = powerMapping[(powerTypes)power];
+		currentPower.Ready();
+	}
+
+	// Called by the server to activate a power
+	public void ActivatePower()
+	{
+		currentPower.Activate();
+	}
 }
