683e8e85dbd16cfd6539a1d9591d1de4113ed14c
Stewart Martin <stewartjmartin@gmail.com>
2017-04-26 01:47:22 -0500

    Making progress on the Main Menu.  Local play stuff should be all hooked up, but it's giving me an error.

---
+++ Assets/scripts/ui/GameSelectionMenuUGUI.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameSelectionMenuUGUI : MonoBehaviour {
+    private LoadAssets loadAssets;
+    public Text tableSelectionText;
+    public Text variantSelectionText;
+    public Text paddleSelectionText;
+    //private int currentTable;
+    //public string selectionType;
+    private int indexLength;
+
+    public Dictionary<string, int> currentIndex = new Dictionary<string, int>() {
+        { "table", 0 },
+        { "variant", 0 },
+        { "paddle", 0 },
+    };
+
+
+
+    void Start()
+    {
+        loadAssets = GetComponent<LoadAssets>();
+        foreach (KeyValuePair<string, int> selectionType in currentIndex)
+        {
+            SetSelectionText(selectionType.Key, loadAssets.GetName(selectionType.Key, currentIndex[selectionType.Key]));
+
+        }
+
+
+    }
+
+
+    public void ForwardButton(string selectionType)
+    {
+        indexLength = loadAssets.GetCount(selectionType) - 1;
+        if (currentIndex[selectionType] < indexLength)
+        {
+            currentIndex[selectionType]++;
+        }
+        else
+        {
+            currentIndex[selectionType] = 0;
+        }
+        SetSelectionText(selectionType, loadAssets.GetName(selectionType, currentIndex[selectionType]));
+
+    }
+
+    public void BackButton(string selectionType)
+    {
+        indexLength = loadAssets.GetCount(selectionType) - 1;
+        if (currentIndex[selectionType] < indexLength)
+        {
+            currentIndex[selectionType] = indexLength;
+        }
+        else
+        {
+            currentIndex[selectionType]--;
+        }
+        SetSelectionText(selectionType, loadAssets.GetName(selectionType, currentIndex[selectionType]));
+    }
+
+    public void MakeSelection()
+    {
+        Debug.Log("Stuff is sort of working!!!!");
+        //GetComponent.makeSelectionText.text
+        //LoadScene
+        loadAssets.LoadScene(currentIndex["table"], currentIndex["variant"], currentIndex["paddle"]);
+        if (loadAssets.HasFinishedLoading() == true)
+        {
+            loadAssets.BeginPlaying();
+        }
+    }
+
+    private void SetSelectionText(string selectionType, string selectionText)
+    {
+        switch (selectionType)
+        {  
+            case "table" : 
+                tableSelectionText.GetComponent<Text>().text = selectionText;
+
+                break;
+            case "variant":
+                variantSelectionText.GetComponent<Text>().text = selectionText;
+                break;
+            case "paddle":
+                paddleSelectionText.GetComponent<Text>().text = selectionText;
+                break;
+            default:
+                Debug.Log("Something went wrong with selectionType");
+                break;
+        }
+    }
+
+
+
+
+}
+
