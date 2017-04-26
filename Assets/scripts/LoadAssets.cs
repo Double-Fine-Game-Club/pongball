4839726cec820d5324958c68b32349b0c2830d13
kednar <kednar@gmail.com>
2017-04-26 12:56:46 +0200

    implemented kednar_table - seems to work

--- Assets/scripts/LoadAssets.cs
+++ Assets/scripts/LoadAssets.cs
@@ -11,13 +11,15 @@ public class LoadAssets : MonoBehaviour {
     // TODO: Can we display dictionaries in the editor??
     public Dictionary<string, bool> variantNames = new Dictionary<string, bool>() {
         { "lightsoda", false },
-        { "kgunn", false }
+        { "kgunn", false },
+	{ "kednar", false }
     };
     public Dictionary<string, bool> tableNames = new Dictionary<string, bool>() {
         { "Table_Clean", false },
         { "Table01", false },
         { "Table02", false },
-        { "Speedster", false }
+        { "Speedster", false },
+	{ "Table_kednar", false }
     };
 
     private string[] activeVariants;
