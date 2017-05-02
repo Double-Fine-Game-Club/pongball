8dd91299b495ce01278fc2003c6ce16027f475c5
Stewart Martin <stewartjmartin@gmail.com>
2017-05-02 02:59:12 -0500

    Added some code that keeps the spinner from being contacted by the ball, more than once per second. I also added some code that lowers the center of mass of the spinner rigid body, by 0.1 on the y axis, which prevents the spinner from being stuck horizontally.

--- Assets/scripts/entity/SpinnerActivator.cs
+++ Assets/scripts/entity/SpinnerActivator.cs
@@ -8,11 +8,21 @@ public class SpinnerActivator : MonoBehaviour
 {
     public bool boostEnabled;
     public GameObject boost;
+    //I'm onlly making this public so I can watch it in the inspector - sjm
+    public bool spinnerCountDown;
+    //declaring this to avoid excess garbage allocation
+    public WaitForSeconds countTime = new WaitForSeconds(1);
+
+    public Vector3 com;
+    public Rigidbody rb;
+
 
     private void Start()
     {
         boost.GetComponent<BoostPadForce>().boostEnabled = false;
         boost.GetComponent<BoostPad>().lightDisabled();
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = com;
     }
 
     private void Awake()
@@ -43,18 +53,31 @@ public class SpinnerActivator : MonoBehaviour
         if (!NetworkManager.singleton.isNetworkActive || NetworkServer.connections.Count > 0)
         {
             //Host only
-            if (boostEnabled == false)
+            if (spinnerCountDown == false)
             {
-                ActivateBoost();
-                GetComponent<ObstacleNetworking>().ActivateOnServer();
-            }
-            else if (boostEnabled == true)
-            {
-                DeactivateBoost();
-                GetComponent<ObstacleNetworking>().ActivateOnServer();
+                StartCoroutine(SpinnerCountDown());
+                if (boostEnabled == false)
+                {
+                    ActivateBoost();
+                    GetComponent<ObstacleNetworking>().ActivateOnServer();
+                }
+                else if (boostEnabled == true)
+                {
+                    DeactivateBoost();
+                    GetComponent<ObstacleNetworking>().ActivateOnServer();
+                }
             }
         }
     }
+
+    //This makes sure that the Spinner trigger is only activated once per second
+    IEnumerator SpinnerCountDown()
+    {
+        spinnerCountDown = true;
+        yield return countTime;
+        spinnerCountDown = false;            
+    }
+
 
     void DeactivateBoost()
     {
